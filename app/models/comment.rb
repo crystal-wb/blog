03a046ad3206d4328c9602a1d6117cf053e69f39
Crystal Williams-Brown <crystal.williams.brown@gmail.com>
2020-04-21 14:24:25 -0400

    showing a better way of adding a foreign key

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  # bin/rails generate migration AddPostToComments post_id:integer:index
+  # bin/rails generate migration AddPostToComments post:references
+  # ls db/migrate
 end
