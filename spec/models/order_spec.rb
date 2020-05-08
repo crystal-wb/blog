bc220ebff168b4b0ce21e8a14976e6b8cf6c2cf6
Crystal Williams-Brown <crystal.williams.brown@gmail.com>
2020-05-08 16:37:24 -0400

    added tests for the orders model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {Order.new(product_name: 'Sprite', product_count: 3, customer: FactoryBot.create(:customer))}
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+  it 'is not valid without a product_name' do
+    subject.product_name = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid without a product_count' do
+    subject.product_count = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid without a customer' do
+    subject.customer_id = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid if the customer_id does not belong to a customer' do
+    subject.customer_id = 0
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid if the product_count is not all digits' do
+    subject.product_count = 'thinkurgr8'
+    expect(subject).to_not be_valid
+  end
 end
